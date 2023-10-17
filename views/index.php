a327acd30eebe1f0f829e5f064ae1d2dc1053ec3
Richard Wyeth <richardwyeth@gmail.com>
2023-10-17 09:34:40 +0200

    Update

--- views/index.php
+++ views/index.php
@@ -1,3 +1,9 @@
 <?php require '../layouts/header.php'; ?>
 
+<div class="landing-video">
+    <video src="../assets/videos/landing.mp4" loop muted autoplay>
+    </video>
+</div>
+
+
 <?php require '../layouts/footer.php'; ?>
